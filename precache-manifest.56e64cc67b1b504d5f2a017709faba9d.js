f2008ec1490150678fe19a25ca0889329574c2fe
hardik.nagda <hardik.nagda@sezzle.com>
2020-05-07 21:44:21 +0530

    Updates

---
+++ precache-manifest.56e64cc67b1b504d5f2a017709faba9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c163cd958b3e338b1c9fc97c136b7679",
+    "url": "/react-hacker-news-clone/index.html"
+  },
+  {
+    "revision": "9cf56e6d21c5bb04b3b4",
+    "url": "/react-hacker-news-clone/static/js/2.00d123bf.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/react-hacker-news-clone/static/js/2.00d123bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a082438648a9533eedbe",
+    "url": "/react-hacker-news-clone/static/js/main.18a583ea.chunk.js"
+  },
+  {
+    "revision": "075347f362713629f0f8",
+    "url": "/react-hacker-news-clone/static/js/runtime-main.48acb3b1.js"
+  }
+]);
